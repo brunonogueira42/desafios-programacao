0bb6c9e87f0756bc835fe53c09ca840094fb599c
Bruno Nogueira <brunonogueira.dev@gmail.com>
2025-03-26 12:52:00 -0300

    Jump Game

---
+++ Semana 1/jumpgame/jumpgame.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+// Complexidade Temporal: O(n)
+// Complexidade Espacial: 0(1)
+
+func canJump(nums []int) bool {
+	maxJump := 0
+
+	for i := 0; i < len(nums); i++ {
+		if i > maxJump {
+			return false
+		}
+
+		if i+nums[i] > maxJump {
+			maxJump = i + nums[i]
+		}
+
+		if maxJump >= len(nums)-1 {
+			return true
+		}
+	}
+
+	return false
+}
+
+func main() {
+	nums1 := []int{2, 3, 1, 1, 4}
+	nums2 := []int{3, 2, 1, 0, 4}
+
+	fmt.Println(canJump(nums1)) // Esperado: true
+	fmt.Println(canJump(nums2)) // Esperado: false
+}
