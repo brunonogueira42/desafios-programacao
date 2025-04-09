1aafc36d6f11958cbab98e1411e620a572bba168
LeonardoDorneles <164582305+LeonardoDorneles@users.noreply.github.com>
2025-04-09 18:01:20 -0300

    build valid parenteses

--- Semana 2/Valid Parentheses/isValid.go
+++ Semana 2/Valid Parentheses/isValid.go
@@ -1,16 +1,27 @@
 package main
 
-import "fmt"
-
-// https://leetcode.com/problems/valid-parentheses/
-
-// Complexidade Temporal:
-// Complexidade Espacial:
-
 func isValid(s string) bool {
-
-}
-
-func main() {
-	fmt.Println()
+	stack := []rune{}
+	for _, ch := range s {
+		switch ch {
+		case '(', '[', '{':
+			stack = append(stack, ch)
+		case ')':
+			if len(stack) == 0 || stack[len(stack)-1] != '(' {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		case ']':
+			if len(stack) == 0 || stack[len(stack)-1] != '[' {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		case '}':
+			if len(stack) == 0 || stack[len(stack)-1] != '{' {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		}
+	}
+	return len(stack) == 0
 }
