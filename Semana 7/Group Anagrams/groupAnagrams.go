7eebefeb31c8ac4e02c7d19617b402f384f91db6
LeonardoDorneles <164582305+LeonardoDorneles@users.noreply.github.com>
2025-05-21 14:47:00 -0300

    build

--- Semana 7/Group Anagrams/groupAnagrams.go
+++ Semana 7/Group Anagrams/groupAnagrams.go
@@ -1,16 +1,35 @@
 package main
 
-import "fmt"
-
-// https://leetcode.com/problems/group-anagrams/
-
-// Complexidade Temporal:
-// Complexidade Espacial:
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
 
 func groupAnagrams(strs []string) [][]string {
+	anagramMap := make(map[string][]string)
 
+	for _, str := range strs {
+		sorted := sortString(str)
+		anagramMap[sorted] = append(anagramMap[sorted], str)
+	}
+
+	result := make([][]string, 0, len(anagramMap))
+	for _, group := range anagramMap {
+		result = append(result, group)
+	}
+
+	return result
+}
+
+func sortString(s string) string {
+	chars := strings.Split(s, "")
+	sort.Strings(chars)
+	return strings.Join(chars, "")
 }
 
 func main() {
-	fmt.Println()
+	strs := []string{"eat", "tea", "tan", "ate", "nat", "bat"}
+	result := groupAnagrams(strs)
+	fmt.Println(result)
 }
--- Semana 7/Isomorphic Strings/isIsomorphic.go
+++ Semana 7/Isomorphic Strings/isIsomorphic.go
@@ -1,16 +1,39 @@
 package main
 
-import "fmt"
-
-// https://leetcode.com/problems/isomorphic-strings/
-
-// Complexidade Temporal:
-// Complexidade Espacial:
+import (
+	"fmt"
+)
 
 func isIsomorphic(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
 
+	mapST := make(map[byte]byte)
+	mapTS := make(map[byte]byte)
+
+	for i := 0; i < len(s); i++ {
+		c1 := s[i]
+		c2 := t[i]
+
+		if val, ok := mapST[c1]; ok {
+			if val != c2 {
+				return false
+			}
+		} else {
+			if _, exists := mapTS[c2]; exists {
+				return false // c2 já está mapeado para outro caractere
+			}
+			mapST[c1] = c2
+			mapTS[c2] = c1
+		}
+	}
+
+	return true
 }
 
 func main() {
-	fmt.Println()
+	fmt.Println(isIsomorphic("egg", "add"))     // true
+	fmt.Println(isIsomorphic("foo", "bar"))     // false
+	fmt.Println(isIsomorphic("paper", "title")) // true
 }
