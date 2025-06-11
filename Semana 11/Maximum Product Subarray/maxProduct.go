aed58a8f04af9da1f9a34ad2ea39910bf3186892
Bruno Nogueira <brunonogueira.dev@gmail.com>
2025-06-11 13:36:02 -0300

    Semana 11

--- Semana 11/Maximum Product Subarray/maxProduct.go
+++ Semana 11/Maximum Product Subarray/maxProduct.go
@@ -4,13 +4,52 @@ import "fmt"
 
 // https://leetcode.com/problems/maximum-product-subarray/
 
-// Complexidade Temporal:
-// Complexidade Espacial:
+// Complexidade Temporal: O(n)
+// Complexidade Espacial: O(1)
 
 func maxProduct(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
 
+	maxProd := nums[0]
+	minProd := nums[0]
+	result := nums[0]
+
+	for i := 1; i < len(nums); i++ {
+		current := nums[i]
+
+		if current < 0 {
+			maxProd, minProd = minProd, maxProd
+		}
+
+		maxProd = max(current, maxProd*current)
+		minProd = min(current, minProd*current)
+
+		result = max(result, maxProd)
+	}
+
+	return result
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
 
 func main() {
-	fmt.Println()
+	nums1 := []int{2, 3, -2, 4}
+	fmt.Println(maxProduct(nums1)) // Esperado: 6
+
+	nums2 := []int{-2, 0, -1}
+	fmt.Println(maxProduct(nums2)) // Esperado: 0
 }
--- Semana 11/Min Cost Climbing Stairs/minCostClimbingStairs.go
+++ Semana 11/Min Cost Climbing Stairs/minCostClimbingStairs.go
@@ -4,13 +4,40 @@ import "fmt"
 
 // https://leetcode.com/problems/min-cost-climbing-stairs/
 
-// Complexidade Temporal:
-// Complexidade Espacial:
+// Complexidade Temporal: O(n)
+// Complexidade Espacial: O(1)
 
 func minCostClimbingStairs(cost []int) int {
+	if len(cost) == 0 {
+		return 0
+	}
+	if len(cost) == 1 {
+		return cost[0]
+	}
 
+	first := cost[0]
+	second := cost[1]
+
+	for i := 2; i < len(cost); i++ {
+		current := min(first, second) + cost[i]
+		first = second
+		second = current
+	}
+
+	return min(first, second)
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
 
 func main() {
-	fmt.Println()
+	cost1 := []int{10, 15, 20}
+	fmt.Println(minCostClimbingStairs(cost1)) // Esperado: 15
+
+	cost2 := []int{1, 100, 1, 1, 1, 100, 1, 1, 100, 1}
+	fmt.Println(minCostClimbingStairs(cost2)) // Esperado: 6
 }
